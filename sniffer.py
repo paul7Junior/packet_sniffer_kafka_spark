64b786d9480aa7110522465979e78e6021a2e00a
paul <paul.hechinger@mediahubml.com>
2021-08-09 19:24:38 +0100

    get packet as proper json - consumed and processed by spark - spark output dataframe

--- sniffer.py
+++ sniffer.py
@@ -1,79 +1,50 @@
+""""
+Sniffer + Kafka producer 
+
+Sniff packet and push it to topic with Kafka Producer
+""""
+
 from scapy.all import sniff
 from kafka import KafkaProducer
 from scapy.all import hexdump, PcapWriter
 import base64
+from json import dumps
 
-
-# class stdout(object):
-#   def write(self, packet):
-#     print("Timestamp {time}, {summary}".format(time=packet.time, summary=packet.summary()))
-#     print("Src IP: {src}, Dst IP: {dst}".format(src=packet['IP'].src, dst=packet['IP'].dst))
-#     hexdump(packet)
-#     print(base64.encodestring(str(packet)))
-
-
-def callback(p):
-  print(p)
-  
-
-class stdout(object):
-  def write(self, packet):
-    print("Timestamp {time}, {summary}".format(time=packet.time, summary=packet.summary()))
-    print("Src IP: {src}, Dst IP: {dst}".format(src=packet['IP'].src, dst=packet['IP'].dst))
-    hexdump(packet)
-    print('£££££££££££££££££££££££££££££')
-    print(packet.__class__)
-    print(str(packet))
-    print(base64.encodebytes(hexdump(packet)))
-
-w = stdout()
-
-
-  
-captured = sniff(iface='', filter='ip', lfilter=lambda x: x.haslayer('IP'),
-          prn=w.write, store=0)
-
-
-
-##########################################
-# basic scapy
-
-def callback(p):
-  return base64.encodestring(str(p).encode('utf-8'))
-
-captured = sniff(iface='', filter='ip', lfilter=lambda x: x.haslayer('IP'),
-          prn=callback, store=1)
-          
-          
-          
-
-          
+   
 ###########################################
 
-class KafkaIn(object):
+    
+def packet_to_json(packet):
+    packet_dict = {}
+    for line in packet:
+        if '###' in line:
+            layer = line.strip('#[] ')
+            packet_dict[layer] = {}
+        elif '=' in line:
+            key, val = line.split('=', 1)
+            packet_dict[layer][key.strip()] = val.strip()
+    print(dumps(packet_dict))
+    return dumps(packet_dict)
+  
+class KafkaProducer(object):
 
     def __init__(self):
         host = '127.0.0.1'
         port = 9092
         server_string = "{host}:{port}".format(host=host, port=port)
-        self.producer = KafkaProducer(bootstrap_servers=server_string)
-    
+        self.producer = KafkaProducer(bootstrap_servers=server_string,
+        value_serializer=lambda x: dumps(packet_to_json(x)).encode())
+        
+        
     def write(self, packet):
-        packet_bytes = base64.encodestring(str(packet).encode())
-        if packet.haslayer('DNS'):
-            future = self.producer.send('quickstart-events', packet_bytes)
-            # future = self.producer.send('catchall', packet_bytes)
-        else:
-            future = self.producer.send('quickstart-events', packet_bytes)
+        packet = packet.show2(dump=True).split('\n')
+        future = self.producer.send('quickstart-events', packet)
         result = future.get(timeout=30)
 
-k = KafkaIn()
+k = KafkaProducer()
 
 captured = sniff(iface='', filter='ip', lfilter=lambda x: x.haslayer('IP'),
-          prn=k.write, store=0)
+          prn=k.write, store=1)
           
-
-
-
 
           
--- spark.py
+++ spark.py
@@ -1,12 +1,17 @@
-##############################################
-# integration with Spark
+"""
+Integration with Spark
+
+ReadStream from Kafka broker. Quick cleaning of input and sink it to the console.
+
+"""
 
 from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 from pyspark.context import SparkContext
-
+from pyspark.sql.functions import from_json, lit, schema_of_json, unbase64, decode, regexp_replace, col
+from pyspark.sql.types import *
 
 
 config = SparkConf() \
@@ -21,27 +26,27 @@ spark = SparkSession \
     .appName("StructuredNetworkWordCount") \
     .getOrCreate()
     
-  
-    
 df = spark \
   .readStream \
   .format("kafka") \
   .option("kafka.bootstrap.servers", "127.0.0.1:9092") \
   .option("subscribe", "quickstart-events") \
   .load()
+ 
+json_str = '{"Ethernet": {"dst": "a4:83:e7:cc:a8:ba", "src": "d4:7b:b0:59:a7:f3", "type": "IPv4"}, "IP": {"version": "4", "ihl": "5", "tos": "0x0", "len": "128", "id": "62122", "flags": "", "frag": "0", "ttl": "51", "proto": "udp", "chksum": "0x2ec5", "src": "170.200.249.55", "dst": "192.168.1.85"}, "UDP": {"sport": "4501", "dport": "55184", "len": "108", "chksum": "0x0"}, "Raw": {"load": "\\"w\\\\n\\\\\\\\xaf6\\\\x00\\\\x00_\\\\\\\\xa1\\\\\\\\x94M\\\\x14i,P\\\\\\\\xdf\\\\x1c\\\\\\\\x8d~2XeT\\\\\\\\xad\\\\\\\\x9e\\\\\\\\x87U\\\\\\\\xa3\\\\\\\\xf1Z\\\\x0f\\\\\\\\x88\\\\x12b\\\\\\\\xf5\\\\\\\\xe6\\\\\\\\xe3\\\\\\\\xf2\\\\\\\\xb7\\\\\\\\x87\\\\\\\\xc5A/op\\\\x06\\\\x18xD\\u0621\\\\x07\\\\\\\\xb5\\\\x16\'\\\\\\\\xbe\\\\\\\\xfc\\u07ff\\\\\\\\xfbH<\\\\x10\\\\x1eF\\\\\\\\xae\\\\x1a\\\\\\\\xa1&\\\\x04\\\\\\\\xd8X\\\\\\\\x98\\\\x02\\\\x1a\\\\\\\\xa7\\\\x19q\\\\\\\\xe3\\\\n3\\\\\\\\x92\\\\\\\\x96\\\\\\\\xdf\\\\\\\\xf5\\\\x18\\\\\\\\xdcrgS\\\\\\\\xebw\\\\\\\\xa2\\\\x01E\\\\\\\\xf6\\u0139{\\\\x0c\\\\\\\\xcf\\""}}'
+json_schema = schema_of_json(lit(json_str))
   
-df = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+clean_input = df \
+.withColumn("value", regexp_replace(col("value").cast("string"), "\\\\", "")) \
+.withColumn("value", regexp_replace("value", "^\"|\"$", "")) \
+.select(from_json("value", json_schema).alias("opc")) \
+.select('opc.*')
 
-df = df.groupBy("value").count()
-
-# df = df.select(bin(df.age).alias('c')).collect()
-
-query = df \
+query = clean_input \
     .writeStream \
-    .outputMode("complete") \
+    .outputMode("update") \
     .format("console") \
     .start()
 
 query.awaitTermination()
   
-# df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
